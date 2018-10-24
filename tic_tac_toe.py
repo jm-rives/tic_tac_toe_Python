05f930120215ce26378eeba2b593de3cc67d2dd2
Joanna M. Rives <joanna.rives@gmail.com>
2018-10-24 14:47:42 -0700

    ADD working game_board update logic.

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,8 +1,14 @@
 def game_board(piece, x, y):
     """the game board is a 2d list that holds the game logic
-        accepts as arguments the game piece and coordinates
+        accepts as arguments the game piece
+        and coordinates ( x == index in list 1, y == index in nested list)
         on game board"""
+    logic_board = [[[], [], []],
+                   [[], [], []],
+                   [[], [], []]
+                   ]
+    logic_board[x][y] = piece
 
+    return logic_board
 
-
-
+print(game_board('X', 2, 2))
