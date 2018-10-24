786b7626cd21344bab7d4871e881f2186468baee
Joanna M. Rives <joanna.rives@gmail.com>
2018-10-24 13:52:26 -0700

    ADD empty game_board function.

---
+++ test_tic_tac_toe.py
@@ -0,0 +1,8 @@
+import pytest
+import tic_tac_toe
+
+# test if game engine exist
+# test if new game displays empty board
+# test if should be an array
+# test if display board exists
+# test if game logice board exists
---
+++ tic_tac_toe.py
@@ -0,0 +1,8 @@
+def game_board(piece, x, y):
+    """the game board is a 2d list that holds the game logic
+        accepts as arguments the game piece and coordinates
+        on game board"""
+
+
+
+
