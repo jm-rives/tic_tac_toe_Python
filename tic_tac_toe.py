c6b009921a866d13ee5d5aef0c5725e198c0a952
Joanna M. Rives <joanna.rives@gmail.com>
2018-10-25 15:37:12 -0700

    ADD display game_board.

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -7,8 +7,11 @@ def game_board(piece, x, y):
                    [[], [], []],
                    [[], [], []]
                    ]
+
     logic_board[x][y] = piece
+    # the board needs to retain it's updated state while game is in play
+    # need a separate function to update?
+    return f"{logic_board[0][0]}|{logic_board[0][1]}|{logic_board[0][2]} \n{logic_board[1][0]}|{logic_board[1][1]}|{logic_board[1][2]} \n{logic_board[2][0]}|{logic_board[2][1]}|{logic_board[2][2]} \n"
 
-    return logic_board
 
-print(game_board('X', 2, 2))
+print(game_board('X', 1, 1))
