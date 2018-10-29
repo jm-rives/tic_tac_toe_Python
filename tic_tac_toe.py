5dcb1fc73a0edb6e47499dd9748983811c79ce6b
Joanna M. Rives <joanna.rives@gmail.com>
2018-10-29 11:24:45 -0700

    ADD empty main function.

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,3 +1,4 @@
+
 def game_board(piece, x, y):
     """the game board is a 2d list that holds the game logic
         accepts as arguments the game piece
@@ -14,7 +15,9 @@ def game_board(piece, x, y):
     return f"{logic_board[0][0]}|{logic_board[0][1]}|{logic_board[0][2]} \n{logic_board[1][0]}|{logic_board[1][1]}|{logic_board[1][2]} \n{logic_board[2][0]}|{logic_board[2][1]}|{logic_board[2][2]} \n"
 
 
-print(game_board('X', 1, 1))
+game_board('X', 1, 1)
 # WILO get logic board state to persist through plays
 # Break off display code into it's own function
 # building game engine may make future step easier
+def main():
+    pass
