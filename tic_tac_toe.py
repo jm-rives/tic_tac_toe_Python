5cb84b6e9ab4e0c7fb40c2ccf78a91791bf28683
Joanna M. Rives <joanna.rives@gmail.com>
2018-10-29 13:47:55 -0700

    ADD board state persistence.

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,27 +1,36 @@
+board = [[[1], [2], [3]],
+            [[4], [5], [6]],
+            [[7], [8], [9]]
+            ]
 
-def game_board(piece, x, y):
+def display_board(board):
+    bar = "." * 13
+    print(f"{board[0][0]}|{board[0][1]}|{board[0][2]}\n{board[1][0]}|{board[1][1]}|{board[1][2]} \n{board[2][0]}|{board[2][1]}|{board[2][2]} \n")
+
+
+def update_board(board, piece, x, y):
     """the game board is a 2d list that holds the game logic
         accepts as arguments the game piece
         and coordinates ( x == index in list 1, y == index in nested list)
         on game board"""
-    logic_board = [[[1], [2], [3]],
-                   [[4], [5], [6]],
-                   [[7], [8], [9]]
-                   ]
 
-    logic_board[x][y] = piece
+
+    board[x][y] = piece
     # the board needs to retain it's updated state while game is in play
     # need a separate function to update?
-    return f"{logic_board[0][0]}|{logic_board[0][1]}|{logic_board[0][2]} \n{logic_board[1][0]}|{logic_board[1][1]}|{logic_board[1][2]} \n{logic_board[2][0]}|{logic_board[2][1]}|{logic_board[2][2]} \n"
+    # Break off display code into it's own function
+    return board
+
 
 
 
 # WILO get logic board state to persist through plays
-# Break off display code into it's own function
-# building game engine may make future step easier
+# building game engine
+board = update_board(board, 'X', 1, 1)
+board = update_board(board, 'O', 0, 1)
+display_board(board)
 def main():
     pass
-
 
 if __name__ == "__main__":
     main()
